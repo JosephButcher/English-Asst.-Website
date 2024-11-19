12ca734a7990f37d679be10952218142144d260f
JosephButcher <liljdbbutcher@gmail.com>
2024-11-19 14:04:42 -0600

    Commit #3

--- english/src/components/AccountComponent.vue
+++ english/src/components/AccountComponent.vue
@@ -1,11 +1,33 @@
 <script setup>
-import NavBar from './NavBarComponent.vue';
+/* import NavBar from './NavBarComponent.vue'; */
 </script>
 
 <template>
-  <div>
-    <h1>Account</h1>
-  </div>
+  <body>
+    <div>
+      <h1>Account</h1>
+    </div>
+    <main>
+      <div>
+        <form method="post">
+          <fieldset>
+            <legend>Account Information</legend>
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname" placeholder="First Name">
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname" placeholder="Last Name">
+            <label for="email">Email address:</label><br>
+            <input type="email" id="email" name="email" placeholder="Email Address">
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" placeholder="Password">
+
+            <input type="submit" value="Submit Answers">
+            </fieldset>
+        </form>
+      </div>
+    </main>
+  </body>
+
 </template>
 
 <style scoped>
